ad4dee7500fd502930994811afb275fcc8d952a5
Austen <aoshaughnessy01@gmail.com>
2022-10-12 15:01:26 -0700

    Finished implementing a way for the user to exit a game while guessing, and added a method to display the user's score.

--- src/main/ui/PasswordApp.java
+++ src/main/ui/PasswordApp.java
@@ -6,6 +6,7 @@ import model.Password;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+// Represents the password application
 public class PasswordApp {
     private Password password;
     private ArrayList<Guess> pastGuesses;
@@ -16,6 +17,8 @@ public class PasswordApp {
         runPassApp();
     }
 
+    // MODIFIES: this
+    // EFFECTS: runs the password app and processes user input
     private void runPassApp() {
         boolean keepRunning = true;
         input = new Scanner(System.in);
@@ -35,19 +38,25 @@ public class PasswordApp {
         }
     }
 
+    //EFFECTS: displays the app's main menu
     private void displayMainMenu() {
         System.out.println("\nMain Menu:");
         System.out.println("\ts --> start new game");
         System.out.println("\tq --> quit");
     }
 
+    // EFFECTS: displays the in-game menu
     private void displayGameMenu() {
         System.out.println("\nTry to guess the password!");
         System.out.println("\tg --> make a guess");
         System.out.println("\td --> show past guesses");
-        System.out.println("\tm --> main menu");
+        System.out.println("\tm --> give up and return to main menu");
     }
 
+    // MODIFIES: this
+    // EFFECTS: runs a new game
+    //          creates a new password for the user to guess
+    //          processes user input related to the game
     private void runNewGame() {
         password = new Password();
         pastGuesses = new ArrayList<>();
@@ -63,33 +72,58 @@ public class PasswordApp {
                     displayPastGuesses();
                     break;
                 case "m":
-                    displayMainMenu();
+                    System.out.println("You have given up on the password" + "\nThe password was "
+                            + password.getPasswordContent());
+                    password.setIsGuessed(true);
                     break;
             }
         }
     }
 
+    // MODIFIES: this
+    // EFFECTS: creates a new guess using the user's input
+    //          compares guess to password
+    //          stops game if password is guessed
     private void makeAGuess() {
         System.out.println("Please enter your " + Password.LENGTH + " character guess:");
         userInput = input.next();
-        Guess currentGuess = new Guess(userInput);
-        currentGuess.compareToPassword(password);
-        if (!password.getIsGuessed()) {
-            currentGuess.updateHint();
-            pastGuesses.add(currentGuess);
-            System.out.println(currentGuess);
+        if (userInput.length() < Password.LENGTH) {
+            System.out.println("Your guess has to be " + Password.LENGTH + " characters long!");
         } else {
-            System.out.println("You have guessed the password!");
+            Guess currentGuess = new Guess(userInput);
+            currentGuess.compareToPassword(password);
+            if (!password.getIsGuessed()) {
+                currentGuess.updateHint();
+                pastGuesses.add(currentGuess);
+                System.out.println(currentGuess);
+            } else {
+                System.out.println("You have guessed the password!");
+                displayScore();
+            }
         }
     }
 
+    // EFFECTS: displays each of the past guesses a user has made
+    //          if no guesses exist, tells the user they have not made any guesses
     private void displayPastGuesses() {
         if (pastGuesses.size() == 0) {
             System.out.println("You have not made any guesses!");
         } else {
+            System.out.println("These are your past guesses:");
             for (Guess pastGuess : pastGuesses) {
                 System.out.println(pastGuess);
             }
         }
+    }
+
+    // EFFECTS: calculates and displays a score
+    private void displayScore() {
+        double score = 100;
+        for (Guess guess: pastGuesses) {
+            if (pastGuesses.indexOf(guess) <= 10) {
+                score -= (double) (1 / 100) * (pastGuesses.indexOf(guess));
+            } // stub
+        }
+        System.out.println("Your score is: " + score);
     }
 }
